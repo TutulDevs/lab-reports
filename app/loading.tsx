a313095edb192d89cfdec32d7e91d8cb158ecfbc
tutul <tutulnahid@gmail.com>
2025-10-06 23:44:12 +0600

    added req files & loader page

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center w-full min-h-svh">
+      <span className="loading loading-spinner loading-xl"></span>
+    </div>
+  );
+}
